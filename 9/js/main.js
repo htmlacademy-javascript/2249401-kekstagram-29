cdafedeacd1c655b5490c48e5d7724211d714216
Keks <keks@htmlacademy.ru>
2023-07-19 23:40:25 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,5 @@
+import { createObjects } from './data.js';
+import { addThumbnail } from './thumbnails.js';
+
+const photos = createObjects();
+addThumbnail(photos);
---
+++ 9/js/thumbnails.js
@@ -0,0 +1,27 @@
+import { openPictureModal } from './modal.js';
+
+const thumbnailElement = document.querySelector('.pictures');
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const createThumbnailElement = ({ url, likes, comments, description }) => {
+  const element = thumbnailTemplate.cloneNode(true);
+  element.querySelector('.picture__img').src = url;
+  element.querySelector('.picture__likes').textContent = likes;
+  element.querySelector('.picture__comments').textContent = comments.length;
+  element.querySelector('.picture__img').alt = description;
+
+
+  // Добавляем обработчик события для открытия модального окна
+  element.addEventListener('click', () => openPictureModal(url, likes, comments, description));
+
+  return element;
+};
+
+const addThumbnail = (similarThumbnails) => {
+  similarThumbnails = Array.from(similarThumbnails);
+  thumbnailElement.querySelectorAll('.picture').forEach((element) => element.remove());
+  const thumbnailElements = similarThumbnails.map(createThumbnailElement);
+  thumbnailElement.append(...thumbnailElements);
+};
+
+export { addThumbnail };
