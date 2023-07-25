d3476e13786a62a0ffebd465f132c0e593235a69
neuvichidmartyn <deemartynov@gmail.com>
2023-07-25 16:51:23 +0300

    предварительное открытие

--- js/form.js
+++ js/form.js
@@ -1,32 +1,65 @@
 import { isEscapeKey } from './utils.js';
 
-const MESSAGE_ERROR_TEXT = '';
+const HASHTAG_MAX_COUNT = 5;
+const TAG_ERROR_TEXT = 'Неправильно введены хештеги';
+const HASHTAG_RULES = /^#[a-zа-яё0-9]{1,19}$/i;
 
-
-const uploadInput = document.querySelector('img-upload__input');
-const uploadOverlay = document.querySelector('img-upload__overlay');
+const form = document.querySelector('.img-upload__form');
+const uploadInput = document.querySelector('.img-upload__input');
+const uploadOverlay = document.querySelector('.img-upload__overlay');
 const uploadCancel = document.querySelector('.img-upload__cancel');
+// const uploadSubmit = document.querySelector('.img-upload__submit');
 
-const onImageUpload = () => {
-  uploadOverlay.classList.remove('hidden');
-  document.body.classList.add('modal-open');
+const hashtagsField = document.querySelector('.text__hashtags');
+const commentField = document.querySelector('.text__description');
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper__error',
+});
+
+const isValidTag = (tag) => HASHTAG_RULES.test(tag);
+const hasValidateCount = (tags) => tags.length <= HASHTAG_MAX_COUNT;
+const hasUniqueTags = (tags) => tags.length === new Set(tags).size;
+
+const validateTags = (value) => {
+  const tags = value.trim().toLowerCase().split(' ');
+  return hasValidateCount(tags) && hasUniqueTags(tags) && tags.every(isValidTag);
 };
 
 const closeUploadedImage = () => {
+  form.reset();
+  pristine.reset();
   uploadOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
 };
 
-const onDocumentKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
+const isElementActive = () => document.activeElement === hashtagsField || document.activeElement === commentField;
+
+function onDocumentKeydown (evt) {
+  if (isEscapeKey(evt) && !isElementActive) {
     evt.preventDefault();
     closeUploadedImage();
   }
-};
+}
 
 const onCloseButtonClick = closeUploadedImage;
-uploadCancel.addEventListener('click', onCloseButtonClick);
-document.addEventListener('keydown', onDocumentKeydown);
 
-uploadInput.addEventListener('change', onImageUpload);
+const onImageUpload = () => {
+  pristine.reset();
+  uploadOverlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
 
+const validateForm = () => {
+
+  pristine.addValidator(hashtagsField, validateTags, TAG_ERROR_TEXT);
+
+  uploadInput.addEventListener('change', onImageUpload);
+  uploadCancel.addEventListener('click', onCloseButtonClick);
+};
+
+export { validateForm };
