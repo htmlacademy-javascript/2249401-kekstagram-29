e138e6375e39e50caaaf7fa7d031bae3f6e157e4
neuvichidmartyn <deemartynov@gmail.com>
2023-07-25 16:54:42 +0300

    --

--- js/scale.js
+++ js/scale.js
@@ -1,4 +1,4 @@
-const MAX_SCALE = 100;
+/* const MAX_SCALE = 100;
 const MIN_SCALE = 25;
 const SCALE_STEP = 25;
 
@@ -6,5 +6,5 @@ const scaleField = document.querySelector('.scale-control__value');
 const scaleMinus = document.querySelector('.scale-control__smaller');
 const scalePlus = document.querySelector('.scale-control__bigger');
 const scalePreview = document.querySelector('.img-upload__preview img');
-
+ */
 
